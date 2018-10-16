93879d399bfe19edaf882fa56408e27308f978e9
Raju <rajireddymattaios@achiever-61.local>
2018-10-16 20:26:44 +0530

    Refer this

--- ccccccc/ccccccc/ViewController.swift
+++ ccccccc/ccccccc/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hi")
+        
+        print("Hello")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
